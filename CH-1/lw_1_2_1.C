7a691d894bda41df4d07109da72401a70b8447d1
dhruvi0308 <dmoradiya74@gmail.com>
2024-06-25 14:56:41 +0530

    Ch-1: Introduction ot C Language

---
+++ CH-1/lw_1_2_1.C
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	clrscr();
+	printf("Name\t:Dhruvi Moradiya\nAge\t:18\nSchool\t:Mount Litera ");
+	getch();
+}
---
+++ CH-1/lw_1_2_2.C
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	clrscr();
+	printf("--------\n|       |\nR       |\nN       |\nW       |\n|       |\n--------");
+	getch();
+}
